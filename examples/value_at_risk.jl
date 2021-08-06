dfe01f67896ec5084ddc3a8b896ac38cb52ba197
mpkuperman <mpkuperman@gmail.com>
2021-08-05 22:30:41 -0300

    working on mixture var

--- examples/value_at_risk.jl
+++ examples/value_at_risk.jl
@@ -1,14 +1,19 @@
 using MarketRisk
 using Distributions
+using ForwardDiff
 
 μ = 0.1
 Σ = 0.2
 ν = 4.13
 N = Normal()
 T = TDist(ν)
+M = MixtureModel(Normal, [(-2.0, 1.2), (0.0, 1.0), (3.0, 2.5)], [0.2, 0.5, 0.3])
+
 
 normal_var = ValueAtRisk(μ, Σ, N)
 student_var = ValueAtRisk(μ, Σ, T) 
+mixture_var = ValueAtRisk(μ, Σ, M) 
 
 compute(normal_var, 10, 0.1)
 compute(student_var, 10, 0.1)
+compute(mixture_var, 10, 0.1)
--- src/value_at_risk.jl
+++ src/value_at_risk.jl
@@ -14,27 +14,31 @@ function ValueAtRisk(portfolio, dist)
     return ParametricValueAtRisk
 end
 
-function compute(var::MarketRisk.ParametricValueAtRisk{V, M, Normal}, h, alpha) where {V, M}
+function compute(var::ParametricValueAtRisk{V, M, D}, h, alpha) where {V, M, D<:Normal}
     @unpack μ, Σ, dist = var 
 
     sqrt(h) * quantile(dist, 1 - alpha) * Σ - h * μ
 end
 
-function compute(var::ParametricValueAtRisk{V, M, TDist}, h, alpha) where {V, M}
+function compute(var::ParametricValueAtRisk{V, M, D}, h, alpha) where {V, M, D<:TDist}
     @unpack μ, Σ, dist = var 
+    @unpack ν = dist
 
-    sqrt((nu - 2) / nu) * sqrt(h) * quantile(dist, 1 - alpha) * Σ - h * μ
+    sqrt((ν - 2) / ν) * sqrt(h) * quantile(dist, 1 - alpha) * Σ - h * μ
 end
 
-# function compute(var, h, alpha)
-#     args = (h, alpha, self.mixture_w, self.pi_mu, self.pi_sigma, self._dist_function)
 
-#     function opt_func(x, t, a, w, m, s, d)
-#         return sum(w[i] * d[i].cdf(x, t * m[i], np.sqrt(t) * s[i]) for i in range(len(w))) - a
-#     end
+function compute(var::ParametricValueAtRisk{V, M, D}, h, alpha) where {V, M, D<:MixtureModel}
+    @unpack μ, Σ, dist = var 
 
-#     return - newton(opt_func, x0=0.0, args=args)
-# end
+    args = (h, alpha, dist)
+
+    function opt_func(x, a)
+        return cdf(dist, x) - a
+    end
+       
+    return - find_zero(opt_func, 0.0, Roots.Newton())
+end
 
 # function compute(h, alpha, nu)
 #     args = (h, alpha, self.mixture_w, self.pi_mu, self.pi_sigma, self._dist_function, nu)
