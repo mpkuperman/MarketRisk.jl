d6bb338ffa75e56377954943da4424ef5c9db600
mpkuperman <mpkuperman@gmail.com>
2022-02-13 15:16:20 +0100

    updated etl example accordingly

--- examples/tstudent_etl.jl
+++ examples/tstudent_etl.jl
@@ -7,12 +7,15 @@ using MarketRisk
 
 h = 10 / 250
 α = 0.01
+αs = [0.01, 0.05, 0.1]
 
 T = TDist(ν)
 
 t_etl = ExpectedTailLoss(μ, Σ, h, α, T)
+t_etl1 = ExpectedTailLoss(μ, Σ, h, αs, T)
 
 compute(t_etl)
+compute(t_etl1)
 
 νs = [5, 10, 15, 20, 25, 10000]
 etls = Float64[]
