f08f04b8cc812da049a609ea277111a4340b43f2
mpkuperman <mpkuperman@gmail.com>
2022-02-13 15:16:05 +0100

    broadcasting for etl

--- examples/normal_etl.jl
+++ examples/normal_etl.jl
@@ -5,9 +5,12 @@ using MarketRisk
 
 h = 10 / 250
 α = 0.01
+αs = [0.01, 0.05, 0.1]
 
 N = Normal()
 
 normal_etl = ExpectedTailLoss(μ, Σ, h, α, N)
+normal_etl1 = ExpectedTailLoss(μ, Σ, h, αs, N)
 
 compute(normal_etl)
+compute(normal_etl1)
--- src/expected_tail_loss.jl
+++ src/expected_tail_loss.jl
@@ -11,35 +11,23 @@ function ExpectedTailLoss(μ, Σ, h, α, distribution)
     return ParametricExpectedTailLoss(μ, Σ, h, α, distribution)
 end
 
-function compute(etl::ParametricExpectedTailLoss{M, S, H, A, D}) where {M<:Real, S<:Real, H<:Real, A<:Real, D<:Normal}
+function compute(etl::ParametricExpectedTailLoss{M, S, H, A, D}) where {M, S, H, A, D<:Normal}
     @unpack μ, Σ, h, α, dist = etl
 
-    xα = quantile(dist, α)
+    xα = @. quantile.(dist, α)
 
-    return 1 / α * pdf(dist, xα) * sqrt(h) * Σ - h * μ
-end
-
-function compute(etl::ParametricExpectedTailLoss{M, S, H, A, D}) where {M<:Real, S<:Real, H<:Real, A<:Array{Real}, D <: Normal}
-    @unpack μ, Σ, h, α, dist = etl
-
-    etl = Float64[]
-    for αᵢ in α
-        xαᵢ = quantile(dist, αᵢ)
-        push!(etl, 1 / αᵢ * pdf(dist, xαᵢ) * sqrt(h) * Σ - h * μ)
-    end
-   
-    return etl
+    @. 1 / α * pdf(dist, xα) * sqrt(h) * Σ - h * μ
 end
 
 function compute(etl::ParametricExpectedTailLoss{M, S, H, A, D}) where {M, S, H, A, D <: TDist}
     @unpack μ, Σ, h, α, dist = etl
     @unpack ν = dist
 
-    xα = quantile(dist, α)
+    xα = @. quantile(dist, α)
 
-    factor = (ν - 2 + xα ^ 2) * (ν - 1) ^ (-1) * α ^ (-1)
+    factor = @. (ν - 2 + xα ^ 2) * (ν - 1) ^ (-1) * α ^ (-1)
 
-    sqrt(h) * factor * pdf(dist, xα) * Σ - h * μ
+    @. sqrt(h) * factor * pdf(dist, xα) * Σ - h * μ
 end
 
 function compute(etl::ParametricExpectedTailLoss{V, M, H, A, MixtureModel{B, D, Normal, F}}) where {V, M, H, A, B, D, F}
